e354c9e1475f9f6e758c0efd1ae715dbb3875e3a
edanur <edanur.karatas505@gmail.com>
2024-10-25 10:36:32 +0300

    menu link setup

--- includes/navbar.php
+++ includes/navbar.php
@@ -14,40 +14,49 @@ include("header.php");
     <li><a href="index.php">Anasayfa</a></li>
     <li><a href="#">Kurumsal&dtrif;</a>
       <ul class="dropdown">
-        <li><a href="pages/kurumsal/development.php">Web Development</a></li>
-        <li><a href="#">Web Design</a></li>
-        <li><a href="#">Illustration</a></li>
-        <li><a href="#">Iconography</a></li>
+     
+        <li><a href="/AduTeknoWeb/pages/kurumsal/mevzuat.php">Mevzuat</a></li>
+        <li><a href="/AduTeknoWeb/pages/kurumsal/teknokentKavramı.php">Teknokent Kavramı</a></li>
+        <li><a href="/AduTeknoWeb/pages/kurumsal/hakimizda.php">Hakkımızda</a></li>
+        <li><a href="/AduTeknoWeb/pages/kurumsal/yonetimKurulu.php">Yönetim Kurulu</a></li>
+        <li><a href="/AduTeknoWeb/pages/kurumsal/yonetimOfisi.php">Yönetim Ofisi</a></li>      
+        <li><a href="/AduTeknoWeb/pages/kurumsal/desteklerimiz.php">Desteklerimiz</a></li>
+        <li><a href="/AduTeknoWeb/pages/kurumsal/sss.php">S.S.S</a></li>
+
       </ul>
     </li>
     <li><a href="#">ADÜ TTO&dtrif;</a>
     <ul class="dropdown">
-        <li><a href="#">Web Development</a></li>
-        <li><a href="#">Web Design</a></li>
-        <li><a href="#">Illustration</a></li>
-        <li><a href="#">Iconography</a></li>
+        <li><a href="/AduTeknoWeb/pages/ADUTTO/girisimcilikVeTicarilesmeBirimi.php">Girişimcilik Ve Ticarileşme Birimi</a></li>
+        <li><a href="/AduTeknoWeb/pages/ADUTTO/fikriHaklarBirimi.php">Fikri Haklar Birimi</a></li>
+        <li><a href="/AduTeknoWeb/pages/ADUTTO/projeDestekBirimi.php">Proje Destek Birimi</a></li>
+        <li><a href="/AduTeknoWeb/pages/ADUTTO/kamuUniversiteSanayiBirimiİsbirligiBirimi.php">Kamu-Üniversite-Sanayi İşbirliği Birimi</a></li>
+        <li><a href="/AduTeknoWeb/pages/ADUTTO/farkındalıkBilgilendirmeVeEgitimHizmetleriBirimi.php">Farkındalık Bilgilendirme Ve Eğitim Hizmetleri Birimi</a></li>
+
       </ul></li>
       <li><a href="#">Başvuru&dtrif;</a>
     <ul class="dropdown">
-        <li><a href="#">Web Development</a></li>
-        <li><a href="#">Web Design</a></li>
-        <li><a href="#">Illustration</a></li>
-        <li><a href="#">Iconography</a></li>
+        <li><a href="/AduTeknoWeb/pages/basvuru/basvuruFormu.php">Başvuru Formu</a></li>
+        <li><a href="/AduTeknoWeb/pages/basvuru/HakemKayit.php">Hakem Kayıt</a></li>        
       </ul></li>
+
       <li><a href="#">Raporlar&dtrif;</a>
     <ul class="dropdown">
-        <li><a href="#">Web Development</a></li>
-        <li><a href="#">Web Design</a></li>
-        <li><a href="#">Illustration</a></li>
-        <li><a href="#">Iconography</a></li>
+        <li><a href="/AduTeknoWeb/pages/raporlar/2021raporu.php">2021 Raporu</a></li>
+        <li><a href="/AduTeknoWeb/pages/raporlar/2020raporu.php">2020 Raporu</a></li>
+        <li><a href="/AduTeknoWeb/pages/raporlar/2019raporu.php">2019 Raporu</a></li>
+        <li><a href="/AduTeknoWeb/pages/raporlar/2018raporu.php">2018 Raporu</a></li>
+        <li><a href="/AduTeknoWeb/pages/raporlar/2017raporu.php">2017 Raporu</a></li>
       </ul></li>
+
     <li><a href="#">Firmalarımız</a></li>
+    
     <li><a href="#">İK&dtrif;</a>
     <ul class="dropdown">
-        <li><a href="#">Web Development</a></li>
-        <li><a href="#">Web Design</a></li>
-        <li><a href="#">Illustration</a></li>
-        <li><a href="#">Iconography</a></li>
+        <li><a href="/AduTeknoWeb/pages/İK/kariyerFırsatları.php">Kariyer Fırsatları</a></li>
+        <li><a href="/AduTeknoWeb/pages/İK/isBasvurusu.php">İş Başvurusu</a></li>
+        <li><a href="/AduTeknoWeb/pages/İK/stajBasvurusu.php">Staj Başvurusu</a></li>
+        
       </ul></li>
       <li><a href="#">İletişim</a></li>
   </ul>
