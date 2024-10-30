5886379ca143446c76dc3023efbeb27db09f1974
edanur <edanur.karatas505@gmail.com>
2024-10-30 12:25:10 +0300

    navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -4,117 +4,41 @@
 include("header.php");
 ?>
 <div class="navbar-container">
-
-  <div class="first-line padd10 ">
-
-    <img src="/AduTeknoWeb/images/aduTto-logo.png" alt="">
-
     
-<<<<<<< HEAD
-    <div class="right-menu">
-
-      <a href="#">Online İşlemler</a>
-
-      <span class="right-span">
-        <p><i class="fas fa-globe-americas "></i> Türkiye</p>
-
-      </span>
-=======
     <div class="first-line"></div>
     <div class="second-line padd10">
        <img src="/AduTeknoWeb/images/indir.png" alt="">
    
        <nav role="navigation" class="primary-navigation">
   <ul>
-    <li><a href="/AduTeknoWeb/index.php">Anasayfa</a></li>
+    <li><a href="index.php">Anasayfa</a></li>
     <li><a href="#">Kurumsal&dtrif;</a>
       <ul class="dropdown">
      
         <li><a href="/AduTeknoWeb/pages/kurumsal/mevzuat.php">Mevzuat</a></li>
-        <li><a href="/AduTeknoWeb/pages/kurumsal/teknoKavram.php">Teknokent Kavramı</a></li>
-        <li><a href="/AduTeknoWeb/pages/kurumsal/hakkimizda.php">Hakkımızda</a></li>
+        <li><a href="/AduTeknoWeb/pages/kurumsal/teknokentKavramı.php">Teknokent Kavramı</a></li>
+        <li><a href="/AduTeknoWeb/pages/kurumsal/hakimizda.php">Hakkımızda</a></li>
         <li><a href="/AduTeknoWeb/pages/kurumsal/yonetimKurulu.php">Yönetim Kurulu</a></li>
         <li><a href="/AduTeknoWeb/pages/kurumsal/yonetimOfisi.php">Yönetim Ofisi</a></li>      
-        <li><a href="/AduTeknoWeb/pages/kurumsal/destek.php">Desteklerimiz</a></li>
+        <li><a href="/AduTeknoWeb/pages/kurumsal/desteklerimiz.php">Desteklerimiz</a></li>
         <li><a href="/AduTeknoWeb/pages/kurumsal/sss.php">S.S.S</a></li>
 
       </ul>
     </li>
     <li><a href="#">ADÜ TTO&dtrif;</a>
     <ul class="dropdown">
-        <li><a href="/AduTeknoWeb/pages/ADUTTO/girisim.php">Girişimcilik Ve Ticarileşme Birimi</a></li>
-        <li><a href="/AduTeknoWeb/pages/ADUTTO/fikriHak.php">Fikri Haklar Birimi</a></li>
-        <li><a href="/AduTeknoWeb/pages/ADUTTO/projeDestek.php">Proje Destek Birimi</a></li>
-        <li><a href="/AduTeknoWeb/pages/ADUTTO/kamuUni.php">Kamu-Üniversite-Sanayi İşbirliği Birimi</a></li>
-        <li><a href="/AduTeknoWeb/pages/ADUTTO/hizmetBirimi.php">Farkındalık Bilgilendirme Ve Eğitim Hizmetleri Birimi</a></li>
+        <li><a href="/AduTeknoWeb/pages/ADUTTO/girisimcilikVeTicarilesmeBirimi.php">Girişimcilik Ve Ticarileşme Birimi</a></li>
+        <li><a href="/AduTeknoWeb/pages/ADUTTO/fikriHaklarBirimi.php">Fikri Haklar Birimi</a></li>
+        <li><a href="/AduTeknoWeb/pages/ADUTTO/projeDestekBirimi.php">Proje Destek Birimi</a></li>
+        <li><a href="/AduTeknoWeb/pages/ADUTTO/kamuUniversiteSanayiBirimiİsbirligiBirimi.php">Kamu-Üniversite-Sanayi İşbirliği Birimi</a></li>
+        <li><a href="/AduTeknoWeb/pages/ADUTTO/farkındalıkBilgilendirmeVeEgitimHizmetleriBirimi.php">Farkındalık Bilgilendirme Ve Eğitim Hizmetleri Birimi</a></li>
 
       </ul></li>
       <li><a href="#">Başvuru&dtrif;</a>
     <ul class="dropdown">
-        <li><a href="/AduTeknoWeb/pages/basvuru/basvuruForm.php">Başvuru Formu</a></li>
-        <li><a href="/AduTeknoWeb/pages/basvuru/hakemKayit.php">Hakem Kayıt</a></li>       
+        <li><a href="/AduTeknoWeb/pages/basvuru/basvuruFormu.php">Başvuru Formu</a></li>
+        <li><a href="/AduTeknoWeb/pages/basvuru/HakemKayit.php">Hakem Kayıt</a></li>        
       </ul></li>
->>>>>>> 0e99cc266b08b3d5f3bbdc9c5be51ca5d2c07055
-
-
-<<<<<<< HEAD
-=======
-    <li><a href="#">Firmalarımız</a></li>
-    
-    <li><a href="#">İK&dtrif;</a>
-    <ul class="dropdown">
-        <li><a href="/AduTeknoWeb/pages/İK/kariyerFirsat.php">Kariyer Fırsatları</a></li>
-        <li><a href="/AduTeknoWeb/pages/İK/isBasvuru.php">İş Başvurusu</a></li>
-        <li><a href="/AduTeknoWeb/pages/İK/stajBasvuru.php">Staj Başvurusu</a></li>
-        
-      </ul></li>
-      <li><a href="#">İletişim</a></li>
-  </ul>
-</nav>
->>>>>>> 0e99cc266b08b3d5f3bbdc9c5be51ca5d2c07055
-    </div>
-
-
-  </div>
-
-
-
-
-  <div class="second-line padd10">
-    <img src="/AduTeknoWeb/images/indir.png" alt="">
-
-    <nav role="navigation" class="primary-navigation">
-      <ul>
-        <li><a href="/AduTeknoWeb/index.php">Anasayfa</a></li>
-        <li><a href="#">Kurumsal&dtrif;</a>
-          <ul class="dropdown">
-
-            <li><a href="/AduTeknoWeb/pages/kurumsal/mevzuat.php">Mevzuat</a></li>
-            <li><a href="/AduTeknoWeb/pages/kurumsal/teknokentKavramı.php">Teknokent Kavramı</a></li>
-            <li><a href="/AduTeknoWeb/pages/kurumsal/hakimizda.php">Hakkımızda</a></li>
-            <li><a href="/AduTeknoWeb/pages/kurumsal/yonetimKurulu.php">Yönetim Kurulu</a></li>
-            <li><a href="/AduTeknoWeb/pages/kurumsal/yonetimOfisi.php">Yönetim Ofisi</a></li>
-            <li><a href="/AduTeknoWeb/pages/kurumsal/desteklerimiz.php">Desteklerimiz</a></li>
-            <li><a href="/AduTeknoWeb/pages/kurumsal/sss.php">S.S.S</a></li>
-
-          </ul>
-        </li>
-        <li><a href="#">ADÜ TTO&dtrif;</a>
-          <ul class="dropdown">
-            <li><a href="/AduTeknoWeb/pages/ADUTTO/girisimcilikVeTicarilesmeBirimi.php">Girişimcilik Ve Ticarileşme Birimi</a></li>
-            <li><a href="/AduTeknoWeb/pages/ADUTTO/fikriHaklarBirimi.php">Fikri Haklar Birimi</a></li>
-            <li><a href="/AduTeknoWeb/pages/ADUTTO/projeDestekBirimi.php">Proje Destek Birimi</a></li>
-            <li><a href="/AduTeknoWeb/pages/ADUTTO/kamuUniversiteSanayiBirimiİsbirligiBirimi.php">Kamu-Üniversite-Sanayi İşbirliği Birimi</a></li>
-            <li><a href="/AduTeknoWeb/pages/ADUTTO/farkındalıkBilgilendirmeVeEgitimHizmetleriBirimi.php">Farkındalık Bilgilendirme Ve Eğitim Hizmetleri Birimi</a></li>
-
-          </ul>
-        </li>
-        <li><a href="#">Başvuru&dtrif;</a>
-          <ul class="dropdown">
-            <li><a href="/AduTeknoWeb/pages/basvuru/basvuruFormu.php">Başvuru Formu</a></li>
-            <li><a href="/AduTeknoWeb/pages/basvuru/HakemKayit.php">Hakem Kayıt</a></li>
-          </ul>
-        </li>
 
         <li><a href="#">Raporlar&dtrif;</a>
           <ul class="dropdown">
@@ -126,18 +50,17 @@ include("header.php");
           </ul>
         </li>
 
-        <li><a href="#">Firmalarımız</a></li>
-
-        <li><a href="#">İK&dtrif;</a>
-          <ul class="dropdown">
-            <li><a href="/AduTeknoWeb/pages/İK/kariyerFırsatları.php">Kariyer Fırsatları</a></li>
-            <li><a href="/AduTeknoWeb/pages/İK/isBasvurusu.php">İş Başvurusu</a></li>
-            <li><a href="/AduTeknoWeb/pages/İK/stajBasvurusu.php">Staj Başvurusu</a></li>
-
-          </ul>
-        </li>
-        <li><a href="#">İletişim</a></li>
-      </ul>
-    </nav>
-  </div>
+    <li><a href="#">Firmalarımız</a></li>
+    
+    <li><a href="#">İK&dtrif;</a>
+    <ul class="dropdown">
+        <li><a href="/AduTeknoWeb/pages/İK/kariyerFırsatları.php">Kariyer Fırsatları</a></li>
+        <li><a href="/AduTeknoWeb/pages/İK/isBasvurusu.php">İş Başvurusu</a></li>
+        <li><a href="/AduTeknoWeb/pages/İK/stajBasvurusu.php">Staj Başvurusu</a></li>
+        
+      </ul></li>
+      <li><a href="#">İletişim</a></li>
+  </ul>
+</nav>
+    </div>
 </div>
